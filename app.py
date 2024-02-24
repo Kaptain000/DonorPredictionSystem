43a7b202751031ed96dc1c1169c2a5bc43794c8a
Kai Gao <kaptain2886@gmail.com>
2024-02-24 17:17:35 -0500

    The web app is ready

--- app.py
+++ app.py
@@ -19,17 +19,44 @@ def home():
 def predict_api():
     data=request.json['data']
     category_features = ['CITY', 'EDUCATION_LEVEL', 'GENDER', 'MARITAL_STATUS', 'OCCUPATION']
+    # transform json data into dataframe
     testdata = pd.DataFrame([data])
+
     feature_arr = oneHotEncoder.transform(testdata[category_features]).toarray()
     feature_labels = oneHotEncoder.categories_
     feature_labels = np.concatenate(feature_labels)
     encoded_df = pd.DataFrame(feature_arr, columns=feature_labels)
     testdata = pd.concat([testdata, encoded_df], axis=1)
     testdata.drop(category_features, axis=1, inplace=True)
+
     testdata['AGE'] = standardScaler.transform(testdata[['AGE']])
+
     output = smote_model.predict(testdata)
     output_serializable = int(output[0])
     return jsonify(output_serializable)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    form_values = request.form.values()
+    testdata = pd.DataFrame([list(form_values)], columns=request.form.keys())
+    print("************************")
+    print(testdata)
+    print("************************")
+    category_features = ['CITY', 'EDUCATION_LEVEL', 'GENDER', 'MARITAL_STATUS', 'OCCUPATION']
+    # transform json data into dataframe
+
+    feature_arr = oneHotEncoder.transform(testdata[category_features]).toarray()
+    feature_labels = oneHotEncoder.categories_
+    feature_labels = np.concatenate(feature_labels)
+    encoded_df = pd.DataFrame(feature_arr, columns=feature_labels)
+    testdata = pd.concat([testdata, encoded_df], axis=1)
+    testdata.drop(category_features, axis=1, inplace=True)
+
+    testdata['AGE'] = standardScaler.transform(testdata[['AGE']])
+
+    output = smote_model.predict(testdata)
+    output_serializable = int(output[0])
+    return render_template("home.html", prediction_text="The donor prediction is {}".format(output_serializable))
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
